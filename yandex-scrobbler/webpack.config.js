25b3f81aae34eb72919a3d942d206826d7a2c00e
Ilya Lityagin <ilityagin@phoenixit.ru>
2025-01-25 03:15:15 +0300

    init

--- yandex-scrobbler/webpack.config.js
+++ yandex-scrobbler/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './background.obfuscated.js', // Your main background script
+  entry: './background.js', // Your main background script
   output: {
     filename: 'background.bundle.js', // Output bundled file
     path: path.resolve(__dirname, 'dist'),
